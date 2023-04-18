da3808b540f6073e9f0dcb49572f3cff3131830f
Evdak <57111036+Evdak@users.noreply.github.com>
2023-04-18 10:54:11 +0300

    fix

--- t_and_d/static/js/offer_admin.js
+++ t_and_d/static/js/offer_admin.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-    setTimeout(() => {
+    setInterval(() => {
         document.querySelectorAll('.select2-selection__choice__display').forEach(el => {
             if (el.textContent.includes('<img')) {
                 el.innerHTML = el.textContent;
@@ -12,8 +12,14 @@ document.addEventListener("DOMContentLoaded", function (event) {
             }
         });
 
+        document.querySelectorAll('.select2-results__option.select2-results__option--selectable').forEach(el => {
+            if (el.textContent.includes('<img')) {
+                el.innerHTML = el.textContent;
+            }
+        });
+
         document.querySelectorAll('.select2-selection.select2-selection--single').forEach(el => {
             el.style.height = 'auto';
         })
-    }, 2000);
+    }, 1000);
 });
